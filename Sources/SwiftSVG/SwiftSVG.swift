a4670e8ff1e095b0a97e654a340e29800b297723
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-02-24 22:06:27 -0500

    Add `SVGData`

--- Sources/SwiftSVG/SwiftSVG.swift
+++ Sources/SwiftSVG/SwiftSVG.swift
@@ -1,2 +1,25 @@
-import svgnative
+//
+//  SwiftSVG.swift
+//  SwiftSVG
+//
+//  Created by Alsey Coleman Miller on 2/24/26.
+//
 
+import Foundation
+import CoreGraphics
+
+/// SVG Data
+public struct SVGData: Equatable, Hashable, Sendable {
+    
+    internal let rawValue: String
+    
+    public init(_ string: String) {
+        self.rawValue = string
+    }
+}
+
+/// SVG Renderer
+public protocol SVGRenderer {
+    
+    func render(_ svg: SVGData, size: CGSize)
+}
